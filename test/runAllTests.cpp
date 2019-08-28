55f737837f2657048392fb0a944976314196be29
alessio <alessiocorsi74@gmail.com>
2019-08-28 11:48:12 +0200

    Add files

---
+++ test/runAllTests.cpp
@@ -0,0 +1,11 @@
+//
+// Created by alessio on 27/08/19.
+//
+
+#include "gtest/gtest.h"
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+return RUN_ALL_TESTS();
+}
+
