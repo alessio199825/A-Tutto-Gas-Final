3f3d4e3389481683883babb267495f62b20ad656
alessio199825 <40171027+alessio199825@users.noreply.github.com>
2019-08-28 11:14:19 +0200

    Add files via upload
    (cherry picked from commit 7008d7f895aa8ba92388234f9c454028a93a7366)

---
+++ Piero.cpp
@@ -0,0 +1,5 @@
+//
+// Created by alessio on 28/08/19.
+//
+
+#include "Piero.h"
---
+++ Piero.h
@@ -0,0 +1,14 @@
+//
+// Created by alessio on 28/08/19.
+//
+
+#ifndef A_TUTTO_GAS_PIERO_H
+#define A_TUTTO_GAS_PIERO_H
+
+
+class Piero {
+
+};
+
+
+#endif //A_TUTTO_GAS_PIERO_H
